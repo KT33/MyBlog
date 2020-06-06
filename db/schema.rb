2ce13a1c186c3120ab5d4458cd26b3788de84e86
KT33 <kt33_antares@yahoo.co.jp>
2020-06-07 00:09:09 +0900

    Add age

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_27_101154) do
+ActiveRecord::Schema.define(version: 2020_06_06_150609) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_27_101154) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.integer "Age"
   end
 
 end
